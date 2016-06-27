139f103cfa01e5573944ad5fc87a4c3498157c13
Sudheesh <Sudheesh@DEVCOK03>
2016-06-27 16:24:20 +0530

    Add project files.

---
+++ DevOpsBot.Base/DevOpsBotArgs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devops.Bot
+{
+    public class DevOpsBotArgs:EventArgs
+    {
+        private Dictionary<string, object> arguments;
+
+        public DevOpsBotArgs()
+        {
+            arguments = new Dictionary<string, object>();
+        }
+
+        public void Add(string key,string value)
+        {
+            arguments.Add(key, value);
+        }
+        public object this[string key]
+        {
+            get
+            {
+                return arguments[key];
+            }
+            set
+            {
+                arguments[key] = value;
+            }
+        }
+    }
+}
---
+++ GitLabAuthenticationProvider/Resources/Projects.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitLabProvider.Resources
+{
+    public class Projects
+    {
+        Dictionary<string, string> InitParams;
+        public void Initialize(Dictionary<string, string> InitilizationParam)
+        {
+            if(!InitilizationParam.ContainsKey(GitLabAuthenticationProvider.AuthenticationParam_BaseProjectURL))
+            {
+                throw new ArgumentNullException("Base_Project_URL", "Dictionary should contain an entry with key AuthenticationParam_BaseProjectURL and value");
+            }
+            InitParams = InitilizationParam;
+        }
+        public async Task<string> ListProjects()
+        {
+            string returnval = string.Empty;
+            if(InitParams == null || InitParams.Count<1)
+            {
+                throw new InvalidOperationException("You Need to call Initialize method before calling this method");
+            }
+
+            return returnval;
+        }
+    }
+}
